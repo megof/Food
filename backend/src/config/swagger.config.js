7dc3ef782ec137773e350b509d31dab0cd699b18
Sebastian Gamez Ariza <juan.gamez1001@gmail.com>
2023-03-24 11:09:27 -0500

    Configuración de swagger

---
+++ backend/src/config/swagger.config.js
@@ -0,0 +1,28 @@
+// Description: This file contains the configuration for swagger
+// Author: Sebastián Gámez Ariza
+
+// Import the swagger-jsdoc
+import swaggerJsDoc from 'swagger-jsdoc';
+
+// Import the port from the environment variables
+import { PORT } from '../config.js';
+
+// Create the swagger options
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Food API Documentation',
+            version: '1.0.0',
+            description: 'This is the documentation for the Food API'
+        },
+        servers: [ { url: `http://localhost:${PORT || 3000}` } ],
+    },
+    apis: [ './src/routes/*.js' ],
+};
+
+// Create the swagger documentation
+const swaggerConfiguration = swaggerJsDoc(swaggerOptions);
+
+// Export the swagger configuration
+export default swaggerConfiguration;
