22f2170a6d16ba2e658361ebb8599886d601b45f
Jean <jean.carrillo@udea.edu.co>
2023-03-27 13:26:02 -0500

    Order, Product and Product Types documentation

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -3,14 +3,164 @@ import * as OrderController from '../controllers/order.controller.js'
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Order:
+ *          type: Object
+ *          properties:
+ *              id:
+ *                  type: string
+ *                  description: auto-generated id of order
+ *              client:
+ *                  type: string
+ *                  description: Name or id of the client
+ *              address: 
+ *                  type: string
+ *                  description: address of the client
+ *              phone:
+ *                  type: string
+ *                  description: Phone of the client
+ *              obs:
+ *                  type: string
+ *                  description: More information about the client
+ *              total:
+ *                  type: number
+ *                  description: Total purchase value
+ *              status:
+ *                  type: string
+ *                  description: En preparación, Listo o En entrega
+ *          required: 
+ *              - client
+ *              - address
+ *              - phone
+ *              - obs
+ *              - total
+ *          example:
+ *              id: 641e1436e5181e37b4d9d326
+ *              client: Jean
+ *              phone: 312836
+ *              obs: Apartamento 801
+ *              total: 19900
+ *              status: En preparación
+ *  parameters:
+ *      orderId:
+ *          in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *              type: string
+ *          description: Id of the order
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Orders
+ *  description: Endpoints of the order
+ */
+
+/**
+ * @swagger
+ *  /api/v1/orders:
+ *      get:
+ *          summary: Get a orders list
+ *          tags: [Orders]
+ *          responses: 
+ *              200:
+ *                  description: the list of orders
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items: 
+ *                                  $ref: '#/components/schemas/Order'
+ * */
 router.get('/', OrderController.getAll);
 
+/**
+ * @swagger
+ *  /api/v1/orders/{id}:
+ *      get:
+ *          summary: Get an order by id
+ *          tags: [Orders]
+ *          parameters:
+ *              - $ref: '#/components/parameters/orderId'
+ *          responses: 
+ *              200:
+ *                  description: Order was found
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Order'
+ *              500:
+ *                  description: Order wasn't found     
+ * */
 router.get('/:id', OrderController.getOne);
 
+/**
+ * @swagger
+ *  /api/v1/orders:
+ *      post:
+ *          summary: Save a new order
+ *          tags: [Orders]
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Order'
+ *          responses: 
+ *              201:
+ *                  description: Order succesfully created
+ *                  content: 
+ *                      application/json:
+ *                          schema:
+ *                              $ref:  '#/components/schemas/Order'
+ *              404:
+ *                  description: Order was not found
+ * */
 router.post('/', OrderController.save);
 
+/**
+ * @swagger
+ *  /api/v1/orders/{id}:
+ *      put:
+ *          summary: Update an order by id
+ *          tags: [Orders]
+ *          parameters:
+ *              - $ref: '#/components/parameters/orderId'
+ *          requestBody:
+ *              required: true
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Order'
+ *          responses:
+ *              200:
+ *                  description: Updated order
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Order'
+ *              500:
+ *                  description: Something wrong with the request.
+ */
 router.put('/:id', OrderController.update);
 
+/**
+ * @swagger
+ *  /api/v1/orders/{id}:
+ *      delete: 
+ *          summary: Delete an order by id
+ *          tags: [Orders]
+ *          parameters:
+ *              - $ref: '#/components/parameters/orderId'
+ *          responses:
+ *              200: 
+ *                  description: the order was deleted
+ */
 router.delete('/:id', OrderController.deleteOne);
 
 export default router;
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -3,14 +3,174 @@ import * as ProductController from '../controllers/product.controller.js'
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Product:
+ *          type: Object
+ *          properties:
+ *              id:
+ *                  type: string
+ *                  description: auto-generated id of Product
+ *              id_tp_product:
+ *                  type: string
+ *                  description: product type id
+ *              name: 
+ *                  type: string
+ *                  description: product's name
+ *              description:
+ *                  type: string
+ *                  description: A shorter product description
+ *              generalDescr:
+ *                  type: string
+ *                  description: A longer product description
+ *              price:
+ *                  type: number
+ *                  description: Price of the product
+ *              image:
+ *                  type: file
+ *                  description: loaded image
+ *              status:
+ *                  type: string
+ *                  description: Disponible or No disponible
+ *              edo:
+ *                  type: boolean
+ *                  description: true or false
+ *          required: 
+ *              - id_tp_product
+ *              - name
+ *              - description
+ *              - generalDescr
+ *              - price
+ *              - edo
+ *          example:
+ *              id: 641e1436e5181e37b4d9d326
+ *              id_tp_product: 641e1436e5181e37b4d9d326
+ *              name: Hamburguesa doble carne
+ *              description: Hamburguesa con doble carne brangus
+ *              generalDescr: Hamburguesa con doble carne brangus, papas fritas, queso, pepinillos y gaseosa a elección.
+ *              price: 19900
+ *              image: /C:/Users/jeanc/OneDrive/Escritorio/Programming/Trabajo/Frontend/restaurante/img/anvorguesa.jpg
+ *              status: Disponible
+ *              edo: true
+ *  parameters:
+ *      productId:
+ *          in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *              type: string
+ *          description: product's id
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Products
+ *  description: Product's endpoints
+ */
+
+/**
+ * @swagger
+ *  /api/v1/products:
+ *      get:
+ *          summary: Get a products list
+ *          tags: [Products]
+ *          responses: 
+ *              200:
+ *                  description: the list of products
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items: 
+ *                                  $ref: '#/components/schemas/Product'
+ * */
 router.get('/', ProductController.getAll);
 
+/**
+ * @swagger
+ *  /api/v1/products/{id}:
+ *      get:
+ *          summary: Get a product by id
+ *          tags: [Products]
+ *          parameters:
+ *              - $ref: '#/components/parameters/productId'
+ *          responses: 
+ *              200:
+ *                  description: Product was found
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Product'
+ *              500:
+ *                  description: Product wasn't found     
+ * */
 router.get('/:id', ProductController.getOne);
 
+/**
+ * @swagger
+ *  /api/v1/products:
+ *      post:
+ *          summary: Save a new product
+ *          tags: [Products]
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          responses: 
+ *              201:
+ *                  description: Product succesfully created
+ *                  content: 
+ *                      application/json:
+ *                          schema:
+ *                              $ref:  '#/components/schemas/Product'
+ *              404:
+ *                  description: Product was not found
+ * */
 router.post('/', ProductController.save);
 
+/**
+ * @swagger
+ *  /api/v1/products/{id}:
+ *      put:
+ *          summary: Update a product by id
+ *          tags: [Products]
+ *          parameters:
+ *              - $ref: '#/components/parameters/productId'
+ *          requestBody:
+ *              required: true
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          responses:
+ *              200:
+ *                  description: Updated product
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Product'
+ *              500:
+ *                  description: Something wrong with the request.
+ */
 router.put('/:id', ProductController.update);
 
+/**
+ * @swagger
+ *  /api/v1/products/{id}:
+ *      delete: 
+ *          summary: Delete a product by id
+ *          tags: [Products]
+ *          parameters:
+ *              - $ref: '#/components/parameters/productId'
+ *          responses:
+ *              200: 
+ *                  description: the product was deleted
+ */
 router.delete('/:id', ProductController.deleteOne);
 
 export default router;
