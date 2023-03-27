b4117f9c78c9fa415f1f9b3531093cdfcf105da7
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-03-27 14:23:49 -0500

    update

---
+++ front/src/components/Lists.vue
@@ -0,0 +1,113 @@
+<template>
+  <div
+    class="container w-300 h-120 d-flex flex-column justify-content-center align-items-center p-2"
+  >
+    <n-space align="center">
+      <n-button color="#ed7902">
+        <n-icon>
+          <Create />
+        </n-icon>
+        Crear
+      </n-button>
+
+ <n-button color="#0066b2">
+    <!--
+        Esto se hace por si le da click que lo envíe donde desee para actualizar
+        @click="actualizarEmpleado(employees._id, employees.name, employees.last, employees.dir, employees.phone, employees.email)" -->
+        <n-icon>
+          <ArrowUpCircle />
+        </n-icon>
+        Actualizar
+      </n-button>
+      
+ <n-button color="#a90b30">
+        <n-icon>
+          <Trash />
+        </n-icon>
+        Eliminar
+      </n-button>
+    </n-space>
+    <n-table class="table table-striped table-hover table-bordered">
+      <thead>
+        <tr>
+          <TH>CLIENTES</TH>
+          <TH>DIRECCIÓN</TH>
+          <TH>TELÉFONO</TH>
+          <TH>OBSERVACIÓN</TH>
+          <TH>FECHA</TH>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Oscar</td>
+          <td>Calle 22</td>
+          <td>31758220</td>
+          <td>Piso 2</td>
+          <td>4/7/2023</td>
+        </tr>
+      </tbody>
+    </n-table>
+  </div>
+</template>
+
+<script>
+import {
+  NList,
+  NListItem,
+  NButton,
+  NTable,
+  NModal,
+  NIcon,
+  NAvatar,
+  NSpace,
+} from "naive-ui";
+import { EyeSharp, Trash, Create,ArrowUpCircle } from "@vicons/ionicons5";
+
+export default {
+  name: "Lists",
+  components: {
+    NList,
+    NListItem,
+    NButton,
+    EyeSharp,
+    NTable,
+    NModal,
+    NIcon,
+    NAvatar,
+    Trash,
+    NSpace,
+    Create,
+    ArrowUpCircle
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    del: {
+      type: Function,
+    },
+  },
+
+  methods: {
+    //   actualizar(_id,name) {
+    // if (this.$route.path === "/ruta de actuliazar") {
+    //   this.$router.push({name:'ruta', params:{id:_id, name: name}});
+    // } else if (this.$route.path === "/ruta de otra persona") {
+    //   this.$router.push({name:'ruta de otra persona', params:{id:_id, name: name}});
+    // }
+    //   },
+  },
+};
+</script>
+
+<style>
+.n-table {
+  text-align: center;
+}
+
+h1 {
+  margin-left: 2%;
+  margin-top: 2%;
+}
+</style>
--- front/src/modules/orders/views/OrdersView.vue
+++ front/src/modules/orders/views/OrdersView.vue
@@ -1,135 +1,22 @@
 <template>
   <h1>Pedidos</h1>
-
-  <div
-    class="container w-300 h-120 d-flex flex-column justify-content-center align-items-center p-2"
-  >
-    <n-table class="table table-striped table-hover table-bordered">
-      <thead>
-        <tr>
-          <TH>CLIENTES</TH>
-          <TH>DIRECCIÓN</TH>
-          <TH>TELÉFONO</TH>
-          <TH>OBSERVACIÓN</TH>
-          <TH>FECHA</TH>
-          <TH>VER</TH>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Oscar</td>
-          <td>Calle 22</td>
-          <td>31758220</td>
-          <td>Piso 2</td>
-          <td>4/7/2023</td>
-          <td>
-            <n-icon-wrapper
-              :size="30"
-              :border-radius="10"
-              @click="showModal = true"
-            >
-              <EyeSharp />
-            </n-icon-wrapper>
-            <n-modal
-              v-model:show="showModal"
-              preset="dialog"
-              style="width: 80%"
-            >
-              <template #header>
-                <div>Toppings</div>
-              </template>
-              <n-table class="table table-striped table-hover table-bordered">
-                <thead>
-                  <tr>
-                    <TH>IMAGEN</TH>
-                    <TH>NOMBRE</TH>
-                    <TH>CANTIDAD</TH>
-                    <TH>PRECIO</TH>
-                    <TH>TOPPING</TH>
-                    <TH>CANTIDAD</TH>
-                    <TH>PRECIO</TH>
-                    <TH>TOTAL</TH>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <n-avatar
-                        round
-                        size="medium"
-                        src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
-                      />
-                    </td>
-                    <td>Perra volteada</td>
-                    <td style="color: blue">2</td>
-                    <td style="color: green">5000</td>
-                    <td>Queso azul</td>
-                    <td style="color: blue">1</td>
-                    <td style="color: green">3000</td>
-                    <td style="color: red">13000</td>
-                  </tr>
-                </tbody>
-              </n-table>
-            </n-modal>
-          </td>
-        </tr>
-      </tbody>
-    </n-table>
-  </div>
+  <Lists> </Lists>
 </template>
 
 <script>
-import {
-  NList,
-  NListItem,
-  NButton,
-  NTable,
-  NModal,
-  NIconWrapper,
-  NAvatar,
-} from "naive-ui";
-import { EyeSharp } from "@vicons/ionicons5";
-import { defineComponent, ref } from "vue";
 
-export default defineComponent({
-  setup() {
-    return {
-      showModal: ref(false),
-    };
-  },
+import Lists from '../../../components/Lists.vue'
+
+export default {
   name: "ListOrders",
-  components: {
-    NList,
-    NListItem,
-    NButton,
-    EyeSharp,
-    NTable,
-    NModal,
-    NIconWrapper,
-    NAvatar,
+  components: { Lists,
   },
-  props: {
-    items: {
-      type: Array,
-      required: true,
-    },
-    del: {
-      type: Function,
-    },
-  },
-  return: {
-    showModal: false,
-  },
-});
+  // return: {
+  //   showModal: false,
+  // },
+};
 </script>
 
-<style>
-.n-table {
-  text-align: center;
-}
 
-h1{
-  margin-left: 2%;
-  margin-top: 2%;
-}
-</style>
+
+
