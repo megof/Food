bd624a8e4eb37cd5474aea34a576a128b34db939
Vanessa <ladyvane1412@gmail.com>
2023-03-27 08:02:03 -0500

    Maquetacion Interfaz de Usuarios en Administrador

--- front/src/modules/users/views/UsersView.vue
+++ front/src/modules/users/views/UsersView.vue
@@ -1,56 +1,201 @@
 <template>
-  <div class="img js-fullheight" style="background-image: url(images/bg.jpg)">
-    <div id="login">
-      <section class="ftco-section">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-md-6 text-center mb-5">
-              <h2 class="heading-section">Iniciar Sesion</h2>
-            </div>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-md-6 col-lg-4">
-              <div class="login-wrap p-0">
-                <div class="signin-form">
-                  <div class="form-group">
-                  </div>
-                  <div class="form-group">
-                    <button type="submit" @click="login" class="form-control btn btn-primary submit px-3">
-                      Iniciar
-                    </button>
-                  </div>
-                  <div class="form-group d-md-flex">
-                    <div class="w-50">
-                      <label class="checkbox-wrap checkbox-primary">Remember Me
-                        <input type="checkbox" checked />
-                        <span class="checkmark"></span>
-                      </label>
+    <header class="w-100 bg-white py-4">
+        <h1>USUARIOS</h1>
+        <ul class="nav justify-content-center bg-white">
+            <li class="nav-item">
+                <a class="nav-link" href="../index.html">Inicio</a>
+            </li>
+        </ul>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Nuevo Usuario
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Agregar Nuevo Usuario</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <!--      <div class="w-50 text-md-right">
-                      <a href="#" style="color: #fff">Forgot Password</a>
-                    </div> -->
-                  </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="text">Nombre:</label>
+                            <input type="text" class="form-control" id="email" v-model="name">
+                          </div>
+                          <div class="form-group">
+                            <label for="text">Usuario:</label>
+                            <input type="text" class="form-control" id="email" v-model="name">
+                          </div>
+                          <div class="form-group">
+                            <label for="text">Clave:</label>
+                            <input type="text" class="form-control" id="email" v-model="name">
+                          </div>
+                          <div class="form-group">
+                            <label for="text">Estado:</label>
+                            <input type="text" class="form-control" id="email" v-model="name">
+                          </div>
+                    </div> 
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button id="btnAdd" type="button" class="btn btn-primary" @click="postMarcas">Agregar</button>
+                    </div>
                 </div>
-                <!--    <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
-                <div class="social d-flex text-center">
-                  <a href="#" class="px-2 py-2 mr-md-1 rounded"
-                    ><span class="ion-logo-facebook mr-2"></span> Facebook</a
-                  >
-                  <a href="#" class="px-2 py-2 ml-md-1 rounded"
-                    ><span class="ion-logo-twitter mr-2"></span> Twitter</a
-                  >
-                </div> -->
-              </div>
             </div>
-          </div>
         </div>
+    </header>
+
+    <section class="container">
+        <table class="table_">
+          <thead>
+            <tr>
+              <th id="first">Id</th>
+              <th>Nombre</th>
+              <th>Usuario</th>
+              <th>Clave</th>
+              <th>Estado</th>
+              <th>Eliminar</th>
+              <th id="last">Actualizar</th>
+            </tr>
+          </thead>
+    
+          <tbody>
+            <tr>
+              <td>Row uno</td>
+              <td>Row dos</td>
+              <td>Row tres</td>
+              <td>Row cuatro</td>
+              <td>Row tres</td>
+              <td><button>Eliminar</button></td>
+              <td><button>Actualizar</button></td>
+            </tr>
+    
+            <tr>
+              <td>Row uno</td>
+              <td>Row dos</td>
+              <td>Row tres</td>
+              <td>Row cuatro</td>
+            </tr>
+          </tbody>
+        </table>
       </section>
-    </div>
-  </div>
 </template>
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from "axios";
 
-<script>
-export default {};
+const marcas = ref([]);
+const name = ref("")
+
+onMounted(()=>{
+     getMarcas();
+     console.log(marcas.value);
+})
+
+
+async function getMarcas() {
+    const res = await fetch("http://localhost:3000/api/v1/brand/all")
+    const data = await res.json();
+    marcas.value = data;
+}
+
+async function postMarcas() {
+    const res = await axios.post("http://localhost:3000/api/v1/brand/create", {name: name.value})
+}
+
+async function deleteMarca(id) {
+    const res = await axios.delete(`http://localhost:3000/api/v1/brand/delete/${id}`)
+}
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap');
 
-<style></style>
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+
+button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    border-radius: 15px;
+    page-break-after: 15 20px;
+    background-image: linear-gradient(to right (#b47474), (#f55d06));
+    color: #080808;
+    font-size: 18px;
+    font-weight: bold;
+    box-shadow: 3px 3px rgb(0, 0, 0, 0.4);
+}
+
+button:hover {
+    box-shadow: 6px 6px rgb(0, 0, 0, 0.8);
+}
+
+.container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 30px;
+    align-items: center;
+
+}
+
+.table_ {
+    background-color: #FAFAFA10;
+    backdrop-filter: blur(0.7rem);
+    text-align: left;
+    border-collapse: collapse;
+    min-width: 600px;
+    border: 2px solid #fafafa10;
+    border-radius: 10px;
+    color: #000;
+    padding: 20px;
+    margin-top: 15px;
+    height: 50vh;
+    overflow: scroll;
+}
+
+#first {
+    border-radius: 10px 0 0 0;
+}
+
+#last {
+    border-radius: 0 10px 0 0;
+
+}
+
+.table_ th,
+td {
+    padding: 20px;
+}
+
+td img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+}
+
+.table_ thead {
+    background-color: #161a42;
+    background-image: linear-gradient(160deg, #161a42 0%, #5462ca 100%);
+    border-bottom: solid 5px rgba(255, 255, 255, .5);
+}
+
+
+.table_ tr:nth-child(even) {
+    background: rgba(0, 0, 0, .1);
+}
+
+.table_ tr:hover td {
+
+    background: rgba(255, 255, 255, .3);
+}
+</style>
