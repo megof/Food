6e965610e862f4edd50388eb59246890c4465182
Juan Camilo <caamilo9517@gmail.com>
2023-03-28 23:57:44 -0500

    Toppings listos y funcionales, integrados con los endpoints

--- front/src/modules/toppings/store/toppings.js
+++ front/src/modules/toppings/store/toppings.js
@@ -2,48 +2,76 @@ import {defineStore} from 'pinia';
 //Importamos los helpers de las peticiones HTTP.
 import fetchData from '../../../helpers/fetchData.js';
 
-const URL= 'https://food-api-market.onrender.com/api/v1/types';
-export const  useTypeStore=defineStore('types',{
+const URL= 'https://food-api-market.onrender.com/api/v1/topping';
+export const useToppingStore=defineStore('toppings',{
     state:()=>({
-        types:[],
+        toppings:[]
+        // toppings:[
+        //     {
+        //         _id:1,
+        //         name:'Papas a la Francesa',
+        //         price:5000,
+        //         edo:false
+        //     },
+        //     {
+        //         _id:2,
+        //         name:'Porción de Arroz',
+        //         price:4000,
+        //         edo:false
+        //     },
+        //     {
+        //         _id:3,
+        //         name:'Porción de Patacón',
+        //         price:4500,
+        //         edo:false
+        //     }
+        // ],
     }),
     actions:{
-        async getTypes(){
-            const {data}=await fetchData(URL);
-           this.types=data;
+        async getToppings(){
+           const {data}=await fetchData(URL);
+           this.toppings=data;
            this.sortById();
         },
         
-        getTypeById(id){
-            const index=this.types.map(el=>el._id).indexOf(id);
-            return this.types[index]; 
+        getToppingById(id){
+            const index=this.toppings.map(el=>el._id).indexOf(id);
+            return this.toppings[index];
         },
         
-        addType(type){
-            this.types.push(type);
+        addTopping(topping){
+            this.toppings.push(topping);
             //Petición HTTP...
-             const data={name:type.name}
+             const data={
+                name:topping.name,
+                price:topping.price,
+             }
              fetchData(URL,'post',data);
             
         },
-        updateType(id,newType){ 
-            const index=this.types.map(el=>el._id).indexOf(id); //El índice que debo alterar.
-            this.types[index]=newType;
-            //Petición HTTP...
+        updateTopping(id,newTopping){ 
+            const index=this.toppings.map(el=>el._id).indexOf(id); //El índice que debo alterar.
+            this.toppings[index]=newTopping;
+            ////Petición HTTP...
             const url=`${URL}/${id}`;
-            const data={name:newType.name};
+            const data={
+                name:newTopping.name,
+                price:newTopping.price,
+                edo:true
+             };
             console.log(data);
             fetchData(url,'put',data); ///PUT
         },
-        deleteType(id){
-            const index=this.types.map(el=>el._id).indexOf(id); //El índice que debo borrar.
-            this.types.splice(index,1);
+        deleteTopping(id){
+            const index=this.toppings.map(el=>el._id).indexOf(id); //El índice que debo borrar.
+            this.toppings.splice(index,1);
             //Petición HTTP...
             const url=`${URL}/${id}`;
             fetchData(url,'delete');
+           
         },
         sortById(){
-            // this.types.sort((a,b)=>a.id-b.id);
+            this.toppings.sort((a,b)=>a.id-b.id);
         }
 
     }
