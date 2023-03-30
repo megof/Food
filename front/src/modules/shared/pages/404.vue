f43c812bfcf144086968c15d850a1b525e4f40a2
jaserleney <yaser.keny@gmail.com>
2023-03-30 03:50:05 -0500

    remove unnecessary files

--- front/src/modules/shared/pages/404.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container-col-12 text-center">
-    <img src="@/assets/img/Error-404.png" alt="error-404" class="w-100" />
-    <router-link :to="{ name: 'home' }" class="btn btn-warning my-3">Llevame al Inicio</router-link>
-  </div>
-</template>
