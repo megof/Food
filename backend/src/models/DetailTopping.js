f076dca01500178e5e21cea574fca4c26c4af4a8
Sebastian Gamez Ariza <juan.gamez1001@gmail.com>
2023-03-30 21:27:31 -0500

    Cambios detailTopping

--- backend/src/models/DetailTopping.js
+++ backend/src/models/DetailTopping.js
@@ -11,7 +11,6 @@ const detailtoppingSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: 'DetailOrder',
             required: true,
-            unique: true,
             trim: true,
         },
         id_topping: {
