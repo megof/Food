6fdb022699ff7a8e11a2d0eea98bf944fc2ebee2
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-03-31 19:00:25 -0500

    Update Lists.vue

--- front/src/modules/orders/components/Lists.vue
+++ front/src/modules/orders/components/Lists.vue
@@ -25,16 +25,15 @@
             <td>{{ top.id_det_order.idOrder.address }}</td>
             <td>{{ top.id_det_order.idOrder.phone }}</td>
             <td>{{ top.id_det_order.idOrder.obs }}</td>
-             <td>{{ formaDate(top.id_det_order.idOrder.createdAt) }}</td>
+            <td>{{ formaDate(top.id_det_order.idOrder.createdAt) }}</td>
             <td>{{ top.id_det_order.idOrder.status }}</td>
 
             <td>
-              <n-button @click="showModals(top._id)" size="large" color=gray>
-                <n-icon size="30" style="margin-top: -6px;">
+              <n-button @click="showModals(top._id)" size="large" color="gray">
+                <n-icon size="30" style="margin-top: -6px">
                   <EyeSharp />
                 </n-icon>
-                <span style="margin-top: 5px; margin-left:4px">ver</span>
-             
+                <span style="margin-top: 5px; margin-left: 4px">ver</span>
               </n-button>
 
               <n-modal
@@ -61,12 +60,17 @@
                     <tr v-if="objectId === top._id">
                       <td>{{ top.id_det_order.idProduct.name }}</td>
                       <td>{{ top.id_det_order.idProduct.generalDescr }}</td>
-                      
+
                       <td style="color: blue">{{ top.id_det_order.cant }}</td>
-                      <td style="color: green">{{ top.id_det_order.idProduct.price }}</td>
-                      <td>{{ top.id_topping.name }}</td>
-                      <td style="color: blue">{{ top.id_det_order.cant }}</td>
-                      <td style="color: green">{{ top.id_topping.price }}</td>
+                      <td style="color: green">
+                        {{ top.id_det_order.idProduct.price }}
+                      </td>
+                      <td v-if="top.id_topping">{{ top.id_topping.name }}</td>
+                      <td v-else>Ninguno</td>
+                      <td v-if="top.id_topping" style="color: blue">{{ top.id_det_order.cant }}</td>
+                      <td v-else style="color: blue">0</td>
+                      <td v-if="top.id_topping" style="color: blue">{{ top.id_topping.price }}</td>
+                      <td v-else style="color: green">0</td>
                     </tr>
                   </tbody>
                 </n-table>
@@ -130,11 +134,11 @@ export default {
       const date = new Date(Date(fecha)).toLocaleDateString();
       return date;
     },
-    showModals(id){
-      this.showModal = true
-      this.objectId = id
-      console.log(id)
-    }
+    showModals(id) {
+      this.showModal = true;
+      this.objectId = id;
+      console.log(id);
+    },
   },
 
   computed: {},
@@ -142,7 +146,7 @@ export default {
     return {
       showModal: false,
       show: false,
-      objectId:undefined
+      objectId: undefined,
     };
   },
 };
