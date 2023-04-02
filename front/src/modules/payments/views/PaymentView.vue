b7e530527b01a4b09d261051b061c92f539c7362
Juan Camilo <caamilo9517@gmail.com>
2023-04-02 15:07:11 -0500

    rama camilo actualizada

--- front/src/modules/payments/views/PaymentView.vue
+++ front/src/modules/payments/views/PaymentView.vue
@@ -5,10 +5,6 @@
                 <Steps/>
             </div>
             <div class="col-md-7 col-12">
-                <!-- <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-dark" @click="prevPinia">Preview</button>
-                    <button type="button" class="btn btn-dark" @click="nextPinia">Next</button>
-                </div> -->
                 <RouterView/>
             </div>
         </div>
@@ -20,9 +16,7 @@
 <script setup>
     import {RouterView} from 'vue-router'
     import Steps from '../components/Steps.vue'
-    import {useStepsStore} from '../store/steps.js'
-    const useSteps=useStepsStore();
-    const{prevPinia,nextPinia}=useSteps;
+ 
 </script>
 
 <style>
