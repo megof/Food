56e5fddf6402d77fecc1fe4cd964eb946048293a
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-04-03 10:31:44 -0500

    homepage update

--- front/src/components/Category.vue
+++ front/src/components/Category.vue
@@ -5,11 +5,7 @@
   > -->
   <div class="container products-container mb-2" v-if="type">
     <span>{{ type.name }}</span>
-    <img
-      class="img-fluid"
-      src="https://www.mcdonalds.com.co/uploads/bigmac_cl_cd9dede2a7.jpg"
-      alt="products"
-    />
+    <img class="img-fluid" :src="type.image.secure_url" alt="products" />
   </div>
 </template>
 
@@ -24,31 +20,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.products-container {
-  width: 15rem;
-  border: 0.125rem solid #e1e1e1;
-  border-left: 0.375rem solid #e1e1e1;
-  box-shadow: 0 0.188rem 0.188rem 0 rgba(0, 0, 0, 0.07);
-  border-radius: 0.375rem;
-  overflow: hidden;
-  transition: 0.3s ease-in-out;
-  flex-shrink: 1;
-}
-
-.products-container:hover {
-  border-left: 0.375rem solid #ffbc0d;
-  cursor: pointer;
-}
-
-@media (max-width: 576px) {
-  .products-container {
-    width: 8rem;
-    font-size: 0.5rem;
-    font-family: "Open Sans";
-    font-weight: 700;
-    transition: 0.3s ease-in-out;
-    margin-bottom: 0.5rem;
-  }
-}
-</style>
+<style scoped></style>
