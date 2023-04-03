5e3804d174ad32df3e538c1549d4ebd1a5412804
jaserleney <yaser.keny@gmail.com>
2023-04-03 00:04:28 -0500

    Push

--- front/src/modules/orders/views/OrdersView.vue
+++ front/src/modules/orders/views/OrdersView.vue
@@ -1,48 +1,41 @@
 <template>
-  <h1>Pedidos</h1>
-  <Lists :columns="columns" :items="items" :show-modal="showModal"/>
+  <div class="container">
+    <h1>Pedidos</h1>
+    <Lists :columns="columns" :items="items" :show-modal="showModal" />
+  </div>
 </template>
 
 <script>
+import Lists from "../components/Lists.vue";
 
-import Lists from '../components/Lists.vue'
+import { NButton, NModal } from "naive-ui";
 
-import {
-  NButton, NModal
-} from "naive-ui";
-
-import fetchData from '@/helpers/fetchData.js'
+import fetchData from "@/helpers/fetchData.js";
 
 export default {
   name: "ListOrders",
-  components: { Lists,NButton,NModal
-  },
+  components: { Lists, NButton, NModal },
   // return: {
   //   showModal: false,
   // },
   data() {
-     return {
-     columns: ['Cliente', 'Dirección', 'teléfono', 'Observación', 'Fecha', 'Estado','opcion'],
-      items: [
-             ],
-      showModal: false
+    return {
+      columns: ["Cliente", "Dirección", "teléfono", "Observación", "Fecha", "Estado", "opcion"],
+      items: [],
+      showModal: false,
     };
   },
-    methods: {
-  async getData(){
-    let url = 'https://food-api-market.onrender.com/api/v1/orders';
-    const {data} = await fetchData(url);
-    this.items = data;
-    console.log(data)
-    console.log(this.items)
-  }
+  methods: {
+    async getData() {
+      let url = "https://food-api-market.onrender.com/api/v1/orders";
+      const { data } = await fetchData(url);
+      this.items = data;
+      console.log(data);
+      console.log(this.items);
+    },
   },
-  created(){
-      this.getData()
-  }
+  created() {
+    this.getData();
+  },
 };
 </script>
-
-
-
-
